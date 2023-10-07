9c908bdb3a5f7078a630cd23be3d6fa25f74f72b
movavi_school <ilusha.chernoglazov@gmail.com>
2023-10-07 20:24:30 +0700

    er

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// import {ButtonComponent} from "./components/button/index.js";
+// const root = document.getElementById('root');
+// const button = new ButtonComponent(root)
+// button.render()
+
+import {ProductCardComponent} from "../../components/product-card/index.js";
+
+// ...
+
+
+const productCard = new ProductCardComponent(this.parent)
+productCard.render()
